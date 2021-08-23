ae67ce28fc50f98eb606b8086ba8eddfef6005ce
Daniel Janis <danieljanis99@gmail.com>
2021-08-23 12:47:45 -0600

    Final touches w/ Further Study

--- data_presenter.py
+++ data_presenter.py
@@ -8,7 +8,22 @@ open_file = open('CupcakeInvoices.csv')
 #         list = line.split(',')
 #     print(list[2])
 
-total_list = []
+# total_list = []
+
+# for line in open_file:
+#     for type in line:
+#         list = line.split(',')
+#     qnt = float(list[3])
+#     price = float(list[4])
+#     total = round(qnt * price, 2)
+#     print(total)
+#     total_list.append(total)
+
+# print(round(sum(total_list), 2))
+
+chocolate_total = []
+vanilla_total = []
+strawberry_total = []
 
 for line in open_file:
     for type in line:
@@ -16,9 +31,19 @@ for line in open_file:
     qnt = float(list[3])
     price = float(list[4])
     total = round(qnt * price, 2)
-    print(total)
-    total_list.append(total)
 
-print(round(sum(total_list), 2))
+    if list[2] == "Chocolate":
+        chocolate_total.append(total)
+    elif list[2] == "Vanilla":
+        vanilla_total.append(total)
+    elif list[2] == "Strawberry":
+        strawberry_total.append(total)
+
+print(round(sum(chocolate_total), 2))
+print(round(sum(vanilla_total), 2))
+print(round(sum(strawberry_total), 2))
+
+
+
 
 open_file.close()
